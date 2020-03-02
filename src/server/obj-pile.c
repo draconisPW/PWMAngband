edcad4f51b78773723c2e6605bfbed4c168a66fd
draconisPW <obenard@generixgroup.com>
2020-03-02 15:47:54 +0100

    Fix bug with gold stacking on floor

--- src/server/obj-pile.c
+++ src/server/obj-pile.c
@@ -452,10 +452,23 @@ bool object_stackable(struct player *p, const struct object *obj1, const struct 
 bool object_similar(struct player *p, const struct object *obj1, const struct object *obj2,
     object_stack_t mode)
 {
-    int total = obj1->number + obj2->number;
+    int total;
 
-    /* Check against stacking limit - except in stores which absorb anyway */
-    if (!(mode & OSTACK_STORE) && (total > obj1->kind->base->max_stack)) return false;
+    /* Hack -- gold */
+    if (tval_is_money(obj1))
+    {
+        total = obj1->pval + obj2->pval;
+
+        /* Check against stacking limit */
+        if (total > PY_MAX_GOLD) return false;
+    }
+    else
+    {
+        total = obj1->number + obj2->number;
+
+        /* Check against stacking limit - except in stores which absorb anyway */
+        if (!(mode & OSTACK_STORE) && (total > obj1->kind->base->max_stack)) return false;
+    }
 
     return object_stackable(p, obj1, obj2, mode);
 }
@@ -594,10 +607,23 @@ void object_absorb_partial(struct object *obj1, struct object *obj2)
  */
 void object_absorb(struct object *obj1, struct object *obj2)
 {
-    int total = obj1->number + obj2->number;
+    int total;
 
-    /* Add together the item counts */
-    obj1->number = MIN(total, obj1->kind->base->max_stack);
+    /* Hack -- gold */
+    if (tval_is_money(obj1))
+    {
+        total = obj1->pval + obj2->pval;
+
+        /* Combine the pvals */
+        obj1->pval = MIN(total, PY_MAX_GOLD);
+    }
+    else
+    {
+        total = obj1->number + obj2->number;
+
+        /* Add together the item counts */
+        obj1->number = MIN(total, obj1->kind->base->max_stack);
+    }
 
     object_absorb_merge(obj1, obj2);
     object_delete(&obj2);
