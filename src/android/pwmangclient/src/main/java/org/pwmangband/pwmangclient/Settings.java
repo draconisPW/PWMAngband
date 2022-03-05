b4c78b31179215992278315a3ac8b85ddb805655
s88100 <71586060+s88100@users.noreply.github.com>
2022-03-06 02:18:15 +0600

    update

--- src/android/pwmangclient/src/main/java/org/pwmangband/pwmangclient/Settings.java
+++ src/android/pwmangclient/src/main/java/org/pwmangband/pwmangclient/Settings.java
@@ -17,6 +17,11 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class Settings extends PWMAngClient {
+    public static void setEnvVars() {
+        SDLActivity.nativeSetenv("HOME", Environment.getExternalStorageDirectory().getAbsolutePath() + "/pwmangclient");
+        SDLActivity.nativeSetenv("ANDROID_APP_PATH", Environment.getExternalStorageDirectory().getAbsolutePath() + "/pwmangclient");
+    }
+
     public static void checkInstall(Context context) {
         File folder = new File(Environment.getExternalStorageDirectory() + File.separator + "pwmangclient");
         if (!folder.exists()) {
@@ -45,16 +50,42 @@ public class Settings extends PWMAngClient {
         }
     }
 
-    public static void setEnvVars() {
-        SDLActivity.nativeSetenv("HOME", Environment.getExternalStorageDirectory().getAbsolutePath() + "/pwmangclient");
-        SDLActivity.nativeSetenv("ANDROID_APP_PATH", Environment.getExternalStorageDirectory().getAbsolutePath() + "/pwmangclient");
-    }
-
     public static void createNewDirectory(String name) {
         //Create a directory before creating a new file inside it.
         File directory = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), name);
         if (!directory.exists()) {
             directory.mkdirs();
+        }
+    }
+
+    public static void copyAssets(Context context, String file, String path) {
+        AssetManager assetManager = context.getAssets();
+        String[] files = null;
+        InputStream in = null;
+        OutputStream out = null;
+        String filename = file;
+        try
+        {
+            in = assetManager.open(filename);
+            out = new FileOutputStream(Environment.getExternalStorageDirectory() + path + File.separator + filename);
+            copyFile(in, out);
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+        }
+        catch(IOException e)
+        {
+            Log.e("tag", "Failed to copy asset file: " + filename, e);
+        }
+    }
+
+    public static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
         }
     }
 
@@ -104,37 +135,6 @@ public class Settings extends PWMAngClient {
             zipInputStream.close();
         } catch (Exception e) {
             Log.e("Decompress", "unzip", e);
-        }
-    }
-
-    public static void copyAssets(Context context, String file, String path) {
-        AssetManager assetManager = context.getAssets();
-        String[] files = null;
-        InputStream in = null;
-        OutputStream out = null;
-        String filename = file;
-        try
-        {
-            in = assetManager.open(filename);
-            out = new FileOutputStream(Environment.getExternalStorageDirectory() + path + File.separator + filename);
-            copyFile(in, out);
-            in.close();
-            in = null;
-            out.flush();
-            out.close();
-            out = null;
-        }
-        catch(IOException e)
-        {
-            Log.e("tag", "Failed to copy asset file: " + filename, e);
-        }
-    }
-
-    public static void copyFile(InputStream in, OutputStream out) throws IOException {
-        byte[] buffer = new byte[1024];
-        int read;
-        while((read = in.read(buffer)) != -1){
-            out.write(buffer, 0, read);
         }
     }
 }
